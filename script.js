f25f50246fba239c4c7f30364a0b10aaef50d0f5
murisaenger-svg <murisaenger@gmail.com>
2026-04-15 16:13:03 -0300

    Add game control functions for starting and exiting

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function startGame() {
+  alert("Iniciando jogo...");
+}
 
+function openOptions() {
+  alert("Abrindo opções...");
+}
+
+function exitGame() {
+  alert("Não dá pra fechar o navegador");
+}
